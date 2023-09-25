690caacdd8890cc58e73984292d201ae8a12406f
mohamed <elzorany.mohamed232@gmail.com>
2023-09-25 14:05:16 +0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,3 +41,30 @@ var swiper = new Swiper(".mySwiper", {
     },
   },
 });
+// ------------
+var swiper = new Swiper(".mysecSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    "@0.75": {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    "@1.00": {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    "@1.50": {
+      slidesPerView: 2,
+      spaceBetween: 50,
+    },
+  },
+});
